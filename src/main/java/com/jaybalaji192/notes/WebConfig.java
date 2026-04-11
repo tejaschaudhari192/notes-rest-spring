be8ee30302ca7026b8d02830ac844751fd60eeda
Tejas <jaybalaji192@gmail.com>
2026-04-11 22:03:06 +0530

    feat: updated origin

--- src/main/java/com/jaybalaji192/notes/WebConfig.java
+++ src/main/java/com/jaybalaji192/notes/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://notes-client-react.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
